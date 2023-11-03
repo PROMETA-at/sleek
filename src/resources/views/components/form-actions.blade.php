584af1eaffe822c6fa02f0b0b40ec5a33f516218
Mario Resl <mario@resl.info>
2023-11-03 12:36:32 +0100

    fix: back button

--- src/resources/views/components/form-actions.blade.php
+++ src/resources/views/components/form-actions.blade.php
@@ -3,7 +3,7 @@
     if (!isset($submit)) $submit = new \Illuminate\View\ComponentSlot();
 @endphp
 <div {{$attributes}}>
-    <button {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }} type="button" data-bs-dismiss="modal">{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</button>
+    <a href="{{url()->previous()}}" {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }}>{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</a>
     <button type="submit" class="btn btn-success" :disabled="loading">
         <span x-show="loading" class="spinner-border spinner-border-sm form-spinner" role="status" aria-hidden="true"></span>
         {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}

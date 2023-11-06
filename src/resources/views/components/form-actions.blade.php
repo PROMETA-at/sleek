f77f406cda13c368b1c4d5fcd61ee7b8d09781f2
Mario Resl <mario@resl.info>
2023-11-06 11:28:21 +0100

    refactor: form-actions extended, so that you can pass with an array which buttons should be displayed; if no array is provided only the submit button will be displayed

--- src/resources/views/components/form-actions.blade.php
+++ src/resources/views/components/form-actions.blade.php
@@ -1,13 +1,28 @@
+@props(['show' => ['submit']])
 @php
     if (!isset($cancel)) $cancel = new \Illuminate\View\ComponentSlot();
     if (!isset($submit)) $submit = new \Illuminate\View\ComponentSlot();
+    if (!isset($reset)) $reset = new \Illuminate\View\ComponentSlot();
 @endphp
 <div {{$attributes}}>
-    <a href="{{url()->previous()}}" {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }}>{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</a>
-    <button type="submit" class="btn btn-success" :disabled="loading">
-        <span x-show="loading" class="spinner-border spinner-border-sm form-spinner" role="status" aria-hidden="true"></span>
-        {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}
-    </button>
+    @if(in_array('reset', $show))
+        <button class="btn btn-outline-warning" type="button" x-on:dblclick="$el.closest('form').reset()" title="Double Click to perform">
+            {{ $reset->attributes->get('label') ?? __('common.actions.reset') }}
+        </button>
+    @endif
+
+    @if(in_array('cancel', $show))
+        <a href="{{url()->previous()}}" {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }}>
+            {{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}
+        </a>
+    @endif
+
+    @if(in_array('submit', $show))
+        <button type="submit" class="btn btn-success" :disabled="loading">
+            <span x-show="loading" class="spinner-border spinner-border-sm form-spinner" role="status" aria-hidden="true"></span>
+            {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}
+        </button>
+    @endif
 </div>
 <style>
     .hidden-spinner{

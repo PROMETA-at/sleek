e3f95d96de8dd0dd9a69dea7c9a6d54787be1294
Florian Proksch <florian.proksch@protonmail.com>
2024-04-22 10:51:00 +0200

    fix: better usage pattern for crud views

--- src/resources/views/components/crud/form.blade.php
+++ src/resources/views/components/crud/form.blade.php
@@ -1,27 +1,25 @@
-@props(['action', 'fields' => [], 'model' => null, 'method' => null, 'header', 'document' => null, 'page' => null])
+@props(['action', 'fields' => [], 'model' => null, 'method' => null, 'header'])
 
-<x-sleek::layout.centered :document="$document" :page="$page">
-  @if (!isset($breadcrumbs) || $breadcrumbs === true)
-    <x-sleek::breadcrumbs />
+@if (!isset($breadcrumbs) || $breadcrumbs === true)
+  <x-sleek::breadcrumbs />
+@endif
+@if (isset($actions))
+  <div class="mb-3">
+    {{ $actions }}
+  </div>
+@endif
+
+<x-sleek::card>
+  @if(isset($header))
+    <x-slot:header>
+      {{ $header }}
+    </x-slot:header>
   @endif
-  @if (isset($actions))
-    <div class="mb-3">
-      {{ $actions }}
-    </div>
-  @endif
 
-  <x-sleek::card>
-    @if(isset($header))
-      <x-slot:header>
-        {{ $header }}
-      </x-slot:header>
-    @endif
-
-    @php
-      $formAttributes = compact('action', 'fields', 'model', 'method');
-    @endphp
-    @component('Prometa\Sleek\Views\Components\EntityForm', 'sleek::entity-form', $formAttributes)
-      @slot('slot', $slot)
-    @endcomponentClass
-  </x-sleek::card>
-</x-sleek::layout.centered>
+  @php
+    $formAttributes = compact('action', 'fields', 'model', 'method');
+  @endphp
+  @component('Prometa\Sleek\Views\Components\EntityForm', 'sleek::entity-form', $formAttributes)
+    @slot('slot', $slot)
+  @endcomponentClass
+</x-sleek::card>
--- src/resources/views/components/crud/list.blade.php
+++ src/resources/views/components/crud/list.blade.php
@@ -1,26 +1,24 @@
-@props(['entities', 'columns', 'document' => null, 'page' => null])
+@props(['entities', 'columns'])
 @php
   if (!isset($table)) $table = new \Illuminate\View\ComponentSlot();
 @endphp
 
-<x-sleek::layout :document="$document" :page="$page">
-  @if (!isset($breadcrumbs) || $breadcrumbs === true)
-    <x-sleek::breadcrumbs />
-  @endif
-  @if (isset($actions))
-    <div class="mb-3">
-      {{ $actions }}
-    </div>
-  @endif
+@if (!isset($breadcrumbs) || $breadcrumbs === true)
+  <x-sleek::breadcrumbs />
+@endif
+@if (isset($actions))
+  <div class="mb-3">
+    {{ $actions }}
+  </div>
+@endif
 
-  @php
-    $tableAttributes =
-      compact('entities', 'columns')
-      + $table->attributes->getAttributes();
-  @endphp
-  @component('Prometa\Sleek\Views\Components\EntityTable', 'sleek::entity-table', $tableAttributes)
-    @foreach(collect($__laravel_slots)->filter(fn ($s, $k) => Str::startsWith($k, 'column')) as $name => $slot)
-      @slot($name, $slot)
-    @endforeach
-  @endcomponentClass
-</x-sleek::layout>
+@php
+  $tableAttributes =
+    compact('entities', 'columns')
+    + $table->attributes->getAttributes();
+@endphp
+@component('Prometa\Sleek\Views\Components\EntityTable', 'sleek::entity-table', $tableAttributes)
+  @foreach(collect($__laravel_slots)->filter(fn ($s, $k) => Str::startsWith($k, 'column')) as $name => $slot)
+    @slot($name, $slot)
+  @endforeach
+@endcomponentClass

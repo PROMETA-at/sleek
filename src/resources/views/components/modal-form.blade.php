f465b843f196f73be42776feb31025b629e8233e
Mario Resl <mario@resl.info>
2024-05-24 10:06:51 +0200

    refactor: adjust modal-form layout to override the footer slot properly

--- src/resources/views/components/modal-form.blade.php
+++ src/resources/views/components/modal-form.blade.php
@@ -11,34 +11,39 @@
                 <h5 class="modal-title">{{ $title }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <x-dynamic-component component="sleek::{{$formType}}" class="modal-body" :action="$action" :method="$method" :model="$model" :fields="$fields" :enctype="$enctype">
-                <div>
+            <x-dynamic-component component="sleek::{{$formType}}" :action="$action" :method="$method" :model="$model" :fields="$fields" :enctype="$enctype">
+                <div class="modal-body">
                     {{ $slot ?? $body }}
                 </div>
-
-                <div class="modal-footer">
-                    <button {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }} type="button" data-bs-dismiss="modal">{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</button>
-                    <button type="submit" class="btn btn-success" :disabled="loading">
-                        <span x-show="loading" class="spinner-border spinner-border-sm modal-spinner hidden-spinner" role="status" aria-hidden="true"></span>
-                        {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}
-                    </button>
-                </div>
+                @if ($footer->isEmpty())
+                    <div class="modal-footer">
+                        <button {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }} type="button" data-bs-dismiss="modal">{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</button>
+                        <button type="submit" class="btn btn-success" :disabled="loading">
+                            <span x-show="loading" class="spinner-border spinner-border-sm modal-spinner hidden-spinner" role="status" aria-hidden="true"></span>
+                            {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}
+                        </button>
+                    </div>
+                @else
+                    <div class="modal-footer">
+                        {{ $footer }}
+                    </div>
+                @endif
             </x-dynamic-component>
         </div>
     </div>
 </div>
 @once
-<style>
-    .hidden-spinner{
-        display: none;
-    }
-</style>
-<script>
-    document.addEventListener('alpine:init', () => {
-        console.log('modal-component: alpine.js loaded');
-        document.querySelectorAll('.modal-spinner').forEach(function(spinner) {
-            spinner.classList.remove('hidden-spinner');
+    <style>
+        .hidden-spinner{
+            display: none;
+        }
+    </style>
+    <script>
+        document.addEventListener('alpine:init', () => {
+            console.log('modal-component: alpine.js loaded');
+            document.querySelectorAll('.modal-spinner').forEach(function(spinner) {
+                spinner.classList.remove('hidden-spinner');
+            });
         });
-    });
-</script>
+    </script>
 @endonce

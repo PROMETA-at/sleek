b145d9ffffa422ce2a1f35f3859f372727ee4b03
Mario Resl <mario@resl.info>
2024-06-03 11:50:53 +0200

    fix: missing layout class

--- src/resources/views/components/page/index.blade.php
+++ src/resources/views/components/page/index.blade.php
@@ -1,9 +1,11 @@
-@if(isset($navbar))
-  {{ $navbar }}
-@elseif(is_string($__data['sleek::navItems'] ?? null))
-  @include($__data['sleek::navItems'])
-@else
-  @include('sleek::layouts.navbar')
-@endif
+<div class="layout">
+    @if(isset($navbar))
+      {{ $navbar }}
+    @elseif(is_string($__data['sleek::navItems'] ?? null))
+      @include($__data['sleek::navItems'])
+    @else
+      @include('sleek::layouts.side-navbar')
+    @endif
 
-{{ $slot }}
+    {{ $slot }}
+</div>

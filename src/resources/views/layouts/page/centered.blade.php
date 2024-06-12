f2cc789ebb225c5c3e4c54f899505f596a9560bb
Mario Resl <mario@resl.info>
2024-06-12 11:37:38 +0200

    chore: remove layout code
    This parts of sleek a no longer used cause of our component solution

--- src/resources/views/layouts/page/centered.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('sleek::layouts.page')
-
-@section('body')
-  <div class="row justify-content-center g-0">
-    <div style="max-width: {{ $size ?? '80ch' }}">
-      @yield('body')
-    </div>
-  </div>
-@overwrite

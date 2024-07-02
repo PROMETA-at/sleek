350c44db296b948fe9ec222418b0a0912148400c
Florian Proksch <florian.proksch@protonmail.com>
2024-07-02 10:56:34 +0200

    chore: phpdoc annotation for sleek facade

--- src/Facades/Sleek.php
+++ src/Facades/Sleek.php
@@ -1,5 +1,14 @@
 <?php namespace Prometa\Sleek\Facades;
 
+/**
+ * @method static void raise(string $message, $type = 'info')
+ * @method static static language(callable | array $data)
+ * @method static static authentication(callable | array | false $data)
+ * @method static static menu(callable | array $data)
+ * @method static static theme(callable | array $data)
+ * @method static static assets(callable | array $data)
+ * @method static static alert(callable | array $data)
+ */
 class Sleek extends \Illuminate\Support\Facades\Facade
 {
   /**

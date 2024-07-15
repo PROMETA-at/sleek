ce116a5a8d8c44ce526d8a39a8e3df69f68e1956
Mario Resl <mario@resl.info>
2024-07-15 12:30:48 +0200

    fix: submitting the modal form

--- src/Views/Components/ModalForm.php
+++ src/Views/Components/ModalForm.php
@@ -6,15 +6,18 @@ use Illuminate\View\Component;
 class ModalForm extends Component
 {
     public function __construct(
+        public $id,
         public $action,
         public $method,
         public $title,
+        public $formId = null,
         public $formType = 'form',
         public $model = null,
         public $fields = null,
         public $enctype = 'application/x-www-form-urlencoded'
     )
     {
+        $this->formId = uniqid();
     }
 
     public function render()
--- src/resources/views/components/modal-form.blade.php
+++ src/resources/views/components/modal-form.blade.php
@@ -4,7 +4,7 @@
     if (!isset($dialog)) $dialog = new \Illuminate\View\ComponentSlot();
     if (!isset($body)) $body = new \Illuminate\View\ComponentSlot();
 @endphp
-<div class="modal fade" {{ $attributes }}>
+<div class="modal fade" id="{{$id}}" {{ $attributes }}>
     <div {{ $dialog->attributes->class(['modal-dialog'])->style(['height: 90%']) }}>
         <div class="modal-content" style="max-height: 100%; overflow-y: auto">
             <div class="modal-header">
@@ -12,14 +12,14 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <x-dynamic-component component="sleek::{{$formType}}" :action="$action" :method="$method" :model="$model" :fields="$fields" :enctype="$enctype">
+                <x-dynamic-component :id="$formId" component="sleek::{{$formType}}" :action="$action" :method="$method" :model="$model" :fields="$fields" :enctype="$enctype">
                     {{ $slot ?? $body }}
                 </x-dynamic-component>
             </div>
             @if (!isset($footer))
                 <div class="modal-footer">
                     <button {{ $cancel->attributes->except(['label'])->class(['btn', 'btn-outline-secondary']) }} type="button" data-bs-dismiss="modal">{{ $cancel->attributes->get('label') ?? __('common.actions.cancel') }}</button>
-                    <button type="submit" class="btn btn-success" :disabled="loading">
+                    <button type="submit" class="btn btn-success" :disabled="loading" form="{{ $formId }}">
                         <span x-show="loading" class="spinner-border spinner-border-sm modal-spinner hidden-spinner" role="status" aria-hidden="true"></span>
                         {{ $submit->attributes->get('label') ?? __('common.actions.submit') }}
                     </button>

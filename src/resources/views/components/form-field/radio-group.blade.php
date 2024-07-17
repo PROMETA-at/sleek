7c4b3ced061e076b6f300d80b29cb6b1edc85736
Florian Proksch <florian.proksch@protonmail.com>
2024-07-17 10:07:30 +0200

    fix: restore radio-group pre-checked functionality

--- src/resources/views/components/form-field/radio-group.blade.php
+++ src/resources/views/components/form-field/radio-group.blade.php
@@ -2,7 +2,7 @@
   <x-slot:input-group style="flex-direction: column"></x-slot:input-group>
   @php($selectedValue = $value)
   @foreach($options as $value => $label)
-    <x-bs::form-check type="radio" {{ $attributes->merge(compact('name', 'value'))->merge(['id' => $id.'-'.$value, 'selected' => $value === $selectedValue]) }}>
+    <x-bs::form-check type="radio" {{ $attributes->merge(compact('name', 'value'))->merge(['id' => $id.'-'.$value, 'checked' => $value === $selectedValue]) }}>
       <x-slot:label>
         {{ $label }}
       </x-slot:label>

1adece116f32f2165531119909c8ce4b3ce02501
Florian Proksch <florian.proksch@protonmail.com>
2024-07-17 10:06:24 +0200

    fix: restore checkbox pre-checked functionality

--- src/resources/views/components/form-field/checkbox.blade.php
+++ src/resources/views/components/form-field/checkbox.blade.php
@@ -2,6 +2,6 @@
   if (is_string($label)) $label = new \Illuminate\View\ComponentSlot($label);
 @endphp
 
-<x-bs::form-check {{ $attributes->merge(compact('id', 'name', 'value')) }}>
+<x-bs::form-check {{ $attributes->merge(compact('id', 'name', 'value'))->merge(['checked' => !!$value]) }}>
   @slot('label', $label, $label->attributes)
 </x-bs::form-check>

471ef80a38a773fb8049a81258a2592321792b0e
Florian Proksch <florian.proksch@protonmail.com>
2024-11-07 21:13:28 +0100

    fix: radio group encodes value if boolean
    This helps with comparing the boolean true/false
    values with the string keys of the options dict.

--- src/resources/views/components/form-field/radio-group.blade.php
+++ src/resources/views/components/form-field/radio-group.blade.php
@@ -1,6 +1,6 @@
 <x-sleek::form-field.wrapper>
   <x-slot:input-group style="flex-direction: column"></x-slot:input-group>
-  @php($selectedValue = $value)
+  @php($selectedValue = is_bool($value) ? json_encode($value) : $value)
   @foreach($options as $value => $label)
     <x-bs::form-check type="radio" {{ $attributes->merge(compact('name', 'value'))->merge(['id' => $id.'-'.$value, 'checked' => $value === $selectedValue]) }}>
       <x-slot:label>

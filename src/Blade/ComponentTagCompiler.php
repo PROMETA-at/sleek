3f7da570c18d1e5b832d01ceb94645fc41ed0e6d
Florian Proksch <florian.proksch@protonmail.com>
2025-03-14 12:10:47 +0100

    feat: relax component/slot attribute parser
    This allows arbitrary variables to be spread into components,
    not just those starting with $attribute.
    Additionally, we now properly merge spread attributes
    with other attributes for slots, such that they can
    be used uniformly.

--- src/Blade/ComponentTagCompiler.php
+++ src/Blade/ComponentTagCompiler.php
@@ -6,6 +6,70 @@ use Illuminate\Support\Str;
 
 class ComponentTagCompiler extends \Illuminate\View\Compilers\ComponentTagCompiler
 {
+    /**
+     * Compile the opening tags within the given string.
+     *
+     * @param  string  $value
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function compileOpeningTags(string $value)
+    {
+        $pattern = "/
+            <
+                \s*
+                x[-\:]([\w\-\:\.]*)
+                (?<attributes>
+                    (?:
+                        \s+
+                        (?:
+                            (?:
+                                @(?:class)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                @(?:style)(\( (?: (?>[^()]+) | (?-1) )* \))
+                            )
+                            |
+                            (?:
+                                \{\{\s*\\\$(?:[^}]+?)?\s*\}\}
+                            )
+                            |
+                            (?:
+                                (\:\\\$)(\w+)
+                            )
+                            |
+                            (?:
+                                [\w\-:.@%]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \'[^\']*\'
+                                        |
+                                        [^\'\\\"=<>]+
+                                    )
+                                )?
+                            )
+                        )
+                    )*
+                    \s*
+                )
+                (?<![\/=\-])
+            >
+        /x";
+
+        return preg_replace_callback($pattern, function (array $matches) {
+            $this->boundAttributes = [];
+
+            $attributes = $this->getAttributesFromAttributeString($matches['attributes']);
+
+            return $this->componentString($matches[1], $attributes);
+        }, $value);
+    }
+
     public function compileSlots(string $value)
     {
         $pattern = "/
@@ -28,7 +92,7 @@ class ComponentTagCompiler extends \Illuminate\View\Compilers\ComponentTagCompil
                             )
                             |
                             (?:
-                                \{\{\s*\\\$attributes(?:[^}]+?)?\s*\}\}
+                                \{\{\s*\\\$(?:[^}]+?)?\s*\}\}
                             )
                             |
                             (?:
@@ -67,6 +131,7 @@ class ComponentTagCompiler extends \Illuminate\View\Compilers\ComponentTagCompil
             $this->boundAttributes = [];
 
             $attributes = $this->getAttributesFromAttributeString($matches['attributes']);
+            $hasSpreadAttributes = array_key_exists('attributes', $attributes);
             $isScoped = array_key_exists('bind', $attributes);
 
             // If an inline name was provided and a name or bound name was *also* provided, we will assume the name should be an attribute...
@@ -76,15 +141,42 @@ class ComponentTagCompiler extends \Illuminate\View\Compilers\ComponentTagCompil
                     : array_merge($attributes, $this->getAttributesFromAttributeString(':name='.$matches['boundName']));
             }
 
+            if ($hasSpreadAttributes) {
+                $spreadValue = $attributes['attributes'];
+                unset($attributes['attributes']);
+                $attributesString = '['.$this->attributesToString($attributes).']';
+                $attributesString = <<<EOF
+                    $spreadValue instanceof Illuminate\View\ComponentAttributeBag
+                    ? {$spreadValue}->merge($attributesString)->getAttributes()
+                    : array_merge($spreadValue ?? [], $attributesString)
+                EOF;
+            } else $attributesString = "[".$this->attributesToString($attributes)."]";
+
             if ($isScoped) {
                 $bindings = trim($attributes['bind'], "'");
                 unset($attributes['bind']);
                 $uses = trim($attributes['use'] ?? '', "'");
                 unset($attributes['use']);
-                return " @slot({$name}, [".$this->attributesToString($attributes)."] bind ({$bindings})" . (strlen($uses) ? " use ({$uses})" : '') . ')';
-            } else return " @slot({$name}, null, [".$this->attributesToString($attributes).']) ';
+                return " @slot({$name}, $attributesString bind ({$bindings})" . (strlen($uses) ? " use ({$uses})" : '') . ')';
+            } else return " @slot({$name}, null, $attributesString)";
         }, $value);
 
         return preg_replace('/<\/\s*x[\-\:]slot[^>]*>/', ' @endslot', $value);
+    }
+
+    /**
+     * Parse the attribute bag in a given attribute string into its fully-qualified syntax.
+     *
+     * @param  string  $attributeString
+     * @return string
+     */
+    protected function parseAttributeBag(string $attributeString)
+    {
+        $pattern = "/
+            (?:^|\s+)                                        # start of the string or whitespace between attributes
+            \{\{\s*(\\\$(?:[^}]+?(?<!\s))?)\s*\}\} # exact match of attributes variable being echoed
+        /x";
+
+        return preg_replace($pattern, ' :attributes="$1"', $attributeString);
     }
 }

92a780cd112e84b013d4f3c0133b90112d09b621
Florian Proksch <florian.proksch@protonmail.com>
2025-07-28 12:46:45 +0200

    feat: integrate sass compilation into interactive dev-server

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Workbench\App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Prometa\Sleek\Facades\Sleek;
 
@@ -14,9 +15,13 @@ class WorkbenchServiceProvider extends ServiceProvider
     {
         Sleek::authentication(false);
         Sleek::assets([
+            'vite' => [
+                'resources/sass/app.scss',
+            ],
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.css',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.js',
             'https://unpkg.com/htmx.org@2.0.4/dist/htmx.js',
+            'https://unpkg.com/hyperscript.org@0.9.14/dist/_hyperscript.js',
         ]);
     }
 
@@ -25,6 +30,7 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vite::useHotFile(__DIR__.'/../../public/hot');
+//        Vite::useBuildDirectory(__DIR__.'/../../public');
     }
 }
--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('overview');
+});
+
+Route::get('/scratchpad', function () {
     return view('scratchpad');
 });
